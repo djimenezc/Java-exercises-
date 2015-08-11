083f5703b0f7a27e7e812f844e86af5831660600
David Jimenez <david.jimenez19@gmail.com>
2015-08-11 23:35:59 +0200

    Hacker rank
    - Java loops

---
+++ src/com/company/hackerrank/Loops.java
@@ -0,0 +1,43 @@
+package com.company.hackerrank;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Loops {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner s = new Scanner(System.in);
+
+        Integer numberSeries = Integer.parseInt(s.nextLine());
+        String tripleSet;
+
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        ArrayList<Integer> b = new ArrayList<Integer>();
+        ArrayList<Integer> n = new ArrayList<Integer>();
+
+        for (int i = 0; i < numberSeries; i++) {
+
+            tripleSet = s.nextLine();
+
+            a.add(Integer.parseInt(tripleSet.split(" ")[0]));
+            b.add(Integer.parseInt(tripleSet.split(" ")[1]));
+            n.add(Integer.parseInt(tripleSet.split(" ")[2]));
+        }
+
+
+        for (int i = 0; i < numberSeries; i++) {
+
+            Double value = Double.valueOf(a.get(i));
+
+            for (int j = 0; j < n.get(i); j++) {
+
+                value = value + b.get(i) * Math.pow(2, j);
+                System.out.print(value.intValue() + " ");
+            }
+
+            System.out.print("\n");
+        }
+    }
+}
