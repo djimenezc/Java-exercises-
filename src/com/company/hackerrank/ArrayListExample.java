e8fcf72d2b8576d652d59723f061f3b0b121b45d
David Jimenez <david.jimenez19@gmail.com>
2015-08-12 18:51:10 +0200

    Hacker rank
    - Array list files created but the problem is not clear and I didn't resolve it

---
+++ src/com/company/hackerrank/ArrayListExample.java
@@ -0,0 +1,50 @@
+package com.company.hackerrank;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
+/**
+ * Created by david on 12/08/2015.
+ */
+public class ArrayListExample {
+
+    public static String FILE_NUMBER = "1";
+    private static final String FILE_FOLDER = "files/arrayListExample/";
+    public static String INPUT_FILE = FILE_FOLDER + "input" + FILE_NUMBER + ".txt";
+    public static String EXPECTED_RESULT_FILE = FILE_FOLDER + "output" + FILE_NUMBER + ".txt";
+    public static String RESULT_FILE = "files/tmp/tmp.txt";
+
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+
+        Scanner s = new Scanner(new File("files/1Darray/input1.txt"));
+        PrintWriter writer = new PrintWriter(RESULT_FILE, "UTF-8");
+
+
+
+
+        writer.close();
+
+        Scanner sOutput = new Scanner(new File(RESULT_FILE));
+        Scanner sExpectedOut = new Scanner(new File(EXPECTED_RESULT_FILE));
+
+        boolean isOutputCorrect = true;
+        int lineNumber = 0;
+
+        while (sOutput.hasNext() && sExpectedOut.hasNext()) {
+
+            String resultLine = sOutput.nextLine();
+            String expectedResultLine = sExpectedOut.nextLine();
+
+            if (!resultLine.equals(expectedResultLine)) {
+                isOutputCorrect = false;
+                System.out.println("Failing in line " + (lineNumber + 1));
+            }
+            lineNumber++;
+        }
+
+        System.out.println("Verification " + isOutputCorrect);
+    }
+}
--- src/com/company/hackerrank/Solution.java
+++ src/com/company/hackerrank/Solution.java
@@ -8,14 +8,24 @@ import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static String FILE_NUMBER = "3";
+    private static final String FILE_FOLDER = "files/1Darray/";
+    public static String INPUT_FILE = FILE_FOLDER + "input" + FILE_NUMBER + ".txt";
+    public static String EXPECTED_RESULT_FILE = FILE_FOLDER + "output" + FILE_NUMBER + ".txt";
+    public static String RESULT_FILE = "files/tmp/tmp.txt";
+
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 
 
 //        Scanner s = new Scanner(new File("files/1Darray/input1.txt"));
+        PrintWriter writer = new PrintWriter(RESULT_FILE, "UTF-8");
         Scanner s = new Scanner(System.in);
         String firstLine = s.nextLine();
         String secondLine = s.nextLine();
         String thirdLine = s.nextLine();
+
+        List<Boolean> resultArray = new ArrayList<Boolean>();
+
 
 
         System.out.println();
@@ -41,5 +51,41 @@ public class Solution {
         for (int i = 0; i < 10; i++) {
 
         }
+
+
+        for (Boolean resultVar : resultArray) {
+
+            if (resultVar) {
+
+                System.out.println("YES");
+                writer.println("YES");
+            } else {
+
+                System.out.println("NO");
+                writer.println("NO");
+            }
+        }
+
+        writer.close();
+
+        Scanner sOutput = new Scanner(new File(RESULT_FILE));
+        Scanner sExpectedOut = new Scanner(new File(EXPECTED_RESULT_FILE));
+
+        boolean isOutputCorrect = true;
+        int lineNumber = 0;
+
+        while (sOutput.hasNext() && sExpectedOut.hasNext()) {
+
+            String resultLine = sOutput.nextLine();
+            String expectedResultLine = sExpectedOut.nextLine();
+
+            if (!resultLine.equals(expectedResultLine)) {
+                isOutputCorrect = false;
+                System.out.println("Failing in line "+ (lineNumber+1));
+            }
+            lineNumber++;
+        }
+
+        System.out.println("Verification " + isOutputCorrect);
     }
 }
