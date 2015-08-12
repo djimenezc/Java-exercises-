3cb536c539f496e1e22c026689b2494fab7a881b
David Jimenez <david.jimenez19@gmail.com>
2015-08-12 12:03:20 +0200

    Hacker rank
    - Big decimals

---
+++ src/com/company/hackerrank/BigDecimalExample2.java
@@ -0,0 +1,35 @@
+package com.company.hackerrank;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BigDecimalExample2 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner s = new Scanner(System.in);
+
+        String firstLine = s.nextLine();
+        ArrayList<BigDecimal> numbersArray = new ArrayList<BigDecimal>();
+
+
+        for (int i = 0; i < Integer.parseInt(firstLine); i++) {
+
+            numbersArray.add(new BigDecimal(s.nextLine()));
+        }
+
+        Collections.sort(numbersArray, Collections.reverseOrder());
+
+        for (BigDecimal number: numbersArray) {
+
+            System.out.println(number.toString().replaceAll("^0.","."));
+        }
+
+
+    }
+}
