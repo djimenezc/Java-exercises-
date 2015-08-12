941f09a48bd9c2d6531344f57bdd56e486927425
David Jimenez <david.jimenez19@gmail.com>
2015-08-12 16:45:04 +0200

    Hacker rank
    - Indicate the line which is different between the expected result and the actual resutl

--- src/com/company/hackerrank/OneDArrayHard.java
+++ src/com/company/hackerrank/OneDArrayHard.java
@@ -10,9 +10,10 @@ import java.util.Scanner;
 
 public class OneDArrayHard {
 
-    public static String FILE_NUMBER = "1";
-    public static String INPUT_FILE = "files/1Darray/input"+FILE_NUMBER+".txt";
-    public static String EXPECTED_RESULT_FILE = "files/1Darray/output"+FILE_NUMBER+".txt";
+    public static String FILE_NUMBER = "2";
+    private static final String FILE_FOLDER = "files/1Darray/";
+    public static String INPUT_FILE = FILE_FOLDER + "input" + FILE_NUMBER + ".txt";
+    public static String EXPECTED_RESULT_FILE = FILE_FOLDER + "output" + FILE_NUMBER + ".txt";
     public static String RESULT_FILE = "files/tmp/tmp.txt";
 
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
@@ -55,6 +56,8 @@ public class OneDArrayHard {
             List<Integer> path = pathArray.get(i);
 
             int position = 0;
+            Boolean backward = false;
+
 
             do {
 
@@ -62,12 +65,15 @@ public class OneDArrayHard {
                     resultArray.add(i, true);
                 } else {
 
-                    if (path.get(position + 1) == 0) {
+                    if (path.get(position + 1) == 0 && !backward) {
                         position += 1;
+                        backward = false;
                     } else if (path.get(position + m) == 0) {
                         position += m;
+                        backward = false;
                     } else if (position - 1 > 0 && path.get(position - 1) == 0) {
                         position -= 1;
+                        backward = true;
                     } else {
                         //not possible to win
                         resultArray.add(i, false);
@@ -106,15 +112,18 @@ public class OneDArrayHard {
         Scanner sExpectedOut = new Scanner(new File(EXPECTED_RESULT_FILE));
 
         boolean isOutputCorrect = true;
+        int lineNumber = 0;
 
         while (sOutput.hasNext() && sExpectedOut.hasNext()) {
 
             String resultLine = sOutput.nextLine();
             String expectedResultLine = sExpectedOut.nextLine();
 
-            if(!resultLine.equals(expectedResultLine)) {
+            if (!resultLine.equals(expectedResultLine)) {
                 isOutputCorrect = false;
+                System.out.println("Failing in line "+ (lineNumber+1));
             }
+            lineNumber++;
         }
 
         System.out.println("Verification " + isOutputCorrect);
