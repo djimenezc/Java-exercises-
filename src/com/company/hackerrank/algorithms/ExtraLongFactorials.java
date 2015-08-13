809e30f941ec02806a1bd3335f2188ed5d3ea532
David Jimenez <david.jimenez19@gmail.com>
2015-08-13 16:45:23 +0200

    Hacker rank
    - extraLongFactorials, finished using recursion in 8:45 min

--- src/com/company/hackerrank/algorithms/ExtraLongFactorials.java
+++ src/com/company/hackerrank/algorithms/ExtraLongFactorials.java
@@ -4,34 +4,43 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class SoluctionMini {
+public class ExtraLongFactorials {
 
     public static String FILE_NUMBER = "1";
-    private static final String FILE_FOLDER = "files/1Darray/";
+    private static final String FILE_FOLDER = "files/extraLongFactorials/";
     public static String INPUT_FILE = FILE_FOLDER + "input" + FILE_NUMBER + ".txt";
     public static String EXPECTED_RESULT_FILE = FILE_FOLDER + "output" + FILE_NUMBER + ".txt";
     public static String RESULT_FILE = "files/tmp/tmp.txt";
+
+
+    private static BigInteger calculateFactorial(Integer n) {
+
+        BigInteger result = n == 1? (BigInteger) BigInteger.valueOf(1) : BigInteger.valueOf(n).multiply
+                (calculateFactorial(n - 1));
+
+        return result;
+    }
 
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 
 
         Scanner s = new Scanner(new File(INPUT_FILE));
 //        Scanner s = new Scanner(System.in);
-        Integer numberEntries =  Integer.parseInt(s.nextLine());
-        String numbers = s.nextLine();
-
-        List<Boolean> resultArray = new ArrayList<Boolean>();
+        Integer n =  Integer.parseInt(s.nextLine());
 
 
-        System.out.println("");
+        BigInteger factorial = calculateFactorial(n);
+
+        System.out.println(factorial);
 
         PrintWriter writer = new PrintWriter(RESULT_FILE, "UTF-8");
 
-        writer.println("a");
+        writer.println(factorial);
 
         writer.close();
 
@@ -55,4 +64,6 @@ public class SoluctionMini {
 
         System.out.println("Verification " + isOutputCorrect);
     }
+
+
 }
