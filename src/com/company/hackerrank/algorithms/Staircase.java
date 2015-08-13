65332c967a05624f2ad103996400c33f91b724d9
David Jimenez <david.jimenez19@gmail.com>
2015-08-13 13:20:30 +0200

    Hacker rank
    - Staircase

--- src/com/company/hackerrank/algorithms/Staircase.java
+++ src/com/company/hackerrank/algorithms/Staircase.java
@@ -5,13 +5,15 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
-public class SoluctionMini {
+public class Staircase {
 
     public static String FILE_NUMBER = "1";
-    private static final String FILE_FOLDER = "files/1Darray/";
+    private static final String FILE_FOLDER = "files/staircase/";
     public static String INPUT_FILE = FILE_FOLDER + "input" + FILE_NUMBER + ".txt";
     public static String EXPECTED_RESULT_FILE = FILE_FOLDER + "output" + FILE_NUMBER + ".txt";
     public static String RESULT_FILE = "files/tmp/tmp.txt";
@@ -19,18 +21,40 @@ public class SoluctionMini {
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 
 
-//        Scanner s = new Scanner(new File(INPUT_FILE));
-        Scanner s = new Scanner(System.in);
-        Integer numberEntries =  Integer.parseInt(s.nextLine());
-        String numbers = s.nextLine();
-
-        List<Boolean> resultArray = new ArrayList<Boolean>();
-
-
-
+        Scanner s = new Scanner(new File(INPUT_FILE));
         PrintWriter writer = new PrintWriter(RESULT_FILE, "UTF-8");
 
-        writer.println("a");
+//        Scanner s = new Scanner(System.in);
+        Integer numberEntries = Integer.parseInt(s.nextLine());
+
+        List<String> resultArray = new ArrayList<String>();
+
+        for (int i = 0; i < numberEntries; i++) {
+            String row = "";
+            for (int j = 0; j < numberEntries; j++) {
+
+                if (i < j + 1) {
+
+                    row += "#";
+                } else {
+                    row += " ";
+                }
+            }
+            resultArray.add(row);
+//            System.out.println(row);
+//            writer.println(row);
+        }
+
+
+        ListIterator<String> iter = resultArray.listIterator(resultArray.size());
+
+        while (iter.hasPrevious()) {
+            String row = iter.previous();
+            System.out.println(row);
+//            writer.println(row);
+        }
+
+
 
         writer.close();
 
@@ -47,7 +71,7 @@ public class SoluctionMini {
 
             if (!resultLine.equals(expectedResultLine)) {
                 isOutputCorrect = false;
-                System.out.println("Failing in line "+ (lineNumber+1));
+                System.out.println("Failing in line " + (lineNumber + 1));
             }
             lineNumber++;
         }
