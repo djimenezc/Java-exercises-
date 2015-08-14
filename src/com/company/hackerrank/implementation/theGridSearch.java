2eeccbdfadeaacef6a2551837dc3b26874db683a
David Jimenez <david.jimenez19@gmail.com>
2015-08-14 18:27:01 +0200

    the grid search example

--- src/com/company/hackerrank/implementation/theGridSearch.java
+++ src/com/company/hackerrank/implementation/theGridSearch.java
@@ -8,13 +8,59 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class theGridSearch {
+public class TheGridSearch {
 
     public static String FILE_NUMBER = "1";
-    private static final String FILE_FOLDER = "files/1Darray/";
+    private static final String FILE_FOLDER = "files/theGridSearch/";
     public static String INPUT_FILE = FILE_FOLDER + "input" + FILE_NUMBER + ".txt";
     public static String EXPECTED_RESULT_FILE = FILE_FOLDER + "output" + FILE_NUMBER + ".txt";
     public static String RESULT_FILE = "files/tmp/tmp.txt";
+
+    private static List<int[]> readGrid(Scanner s, int c, int r) {
+
+        List<int[]> grid = new ArrayList<int[]>();
+
+        for (int j = 0; j < c; j++) {
+            String row = s.nextLine();
+            int[] arrayRow = new int[r];
+            grid.add(arrayRow);
+            for (int k = 0; k < r; k++) {
+                arrayRow[k] = Integer.parseInt(String.valueOf(row.charAt(k)).toLowerCase());
+            }
+        }
+
+        return grid;
+    }
+
+    private static String findPattern(List<int[]> grid, List<int[]> pattern) {
+
+
+        String result = "NO";
+
+        for (int i = 0; i < grid.size() - pattern.size(); i++) {
+            for (int j = 0; j < grid.get(0).length - pattern.get(0).length; j++) {
+                boolean isNotValid = false;
+
+                for (int k = 0; k < pattern.size(); k++) {
+                    if(isNotValid) {
+                        break;
+                    }
+                    for (int m = 0; m < pattern.get(0).length; m++) {
+                        if(grid.get(i + k)[j + m] != pattern.get(k)[m]) {
+                            isNotValid = true;
+                            break;
+                        } else {
+                            if(k + 1 == pattern.size() && m + 1 == pattern.get(0).length) {
+                                return "YES";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
 
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 
@@ -22,18 +68,32 @@ public class theGridSearch {
         Scanner s = new Scanner(new File(INPUT_FILE));
 //        Scanner s = new Scanner(System.in);
         Integer t = Integer.parseInt(s.nextLine());
-        String numbers = s.nextLine();
         String result = "";
 
         List<Boolean> resultArray = new ArrayList<Boolean>();
 
         for (int i = 0; i < t; i++) {
 
+            String numbersGrid = s.nextLine();
+            int c = Integer.parseInt(numbersGrid.split(" ")[0]);
+            int r = Integer.parseInt(numbersGrid.split(" ")[1]);
+
+            List<int[]> grid = readGrid(s, c, r);
+
+            String numbersPattern = s.nextLine();
+            c = Integer.parseInt(numbersPattern.split(" ")[0]);
+            r = Integer.parseInt(numbersPattern.split(" ")[1]);
+
+            List<int[]> pattern = readGrid(s, c, r);
+
+            result = findPattern(grid, pattern);
+
+            System.out.println(result);
+            writer.println(result);
         }
 
 
-        System.out.println(result);
-        writer.println(result);
+
 
         writer.close();
 
@@ -57,4 +117,6 @@ public class theGridSearch {
 
         System.out.println("Verification " + isOutputCorrect);
     }
+
+
 }
